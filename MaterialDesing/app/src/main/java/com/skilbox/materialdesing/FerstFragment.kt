a690a9704cc9273509347894ff7e45860f3e70f6
Anton Gorodetskiy <demox2642@gmail.com>
2021-09-24 21:12:33 +0500

    Create Snackbar

--- MaterialDesing/app/src/main/java/com/skilbox/materialdesing/FerstFragment.kt
+++ MaterialDesing/app/src/main/java/com/skilbox/materialdesing/FerstFragment.kt
@@ -1,16 +1,20 @@
 package com.skilbox.materialdesing
 
 import android.os.Bundle
+import android.os.Handler
 import android.util.Log
 import android.view.View
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.GridLayoutManager
+import com.google.android.material.snackbar.Snackbar
 import com.skilbox.materialdesing.databinding.FragmentFerstBinding
 import com.skilbox.materialdesing.listadapter.ProductListAdapter
 import com.skilbox.materialdesing.plugins.ViewBindingFragment
 import com.skilbox.materialdesing.vm.ProductViewModel
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.fragment_ferst.*
 import kotlinx.coroutines.flow.collectLatest
 
 class FerstFragment : ViewBindingFragment<FragmentFerstBinding>(FragmentFerstBinding::inflate) {
@@ -19,9 +23,20 @@ class FerstFragment : ViewBindingFragment<FragmentFerstBinding>(FragmentFerstBin
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        ProductViewModel().getProduct()
         initList()
         initViewModel()
+
+        Handler().postDelayed(
+            {
+                Snackbar.make(view, "Соединение с сервером отсутствует, показаны сохранённые объекты", Snackbar.LENGTH_INDEFINITE)
+                    .setAction("Повторить") {
+                        Snackbar.make(view, "Список обновлён", Snackbar.LENGTH_LONG)
+                            .show()
+                    }
+                    .show()
+            },
+            3000
+        )
     }
 
     private fun initList() {
